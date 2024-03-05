e97db550ebbcb5fd8378832a00482977834a191c
akaRomal <adfsdf@gmail.com>
2024-03-06 01:34:26 +0500

    Сделал окно через AlertDialog

--- AlertDialog/app/src/main/java/com/school/alertdialog/MainActivity.kt
+++ AlertDialog/app/src/main/java/com/school/alertdialog/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.school.alertdialog
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showAlertDialog()
+    }
+
+    private fun showAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder
+            .setView(R.layout.alert_dialog)
+            .setPositiveButton(R.string.ok_alert_dialog){ dialog, _ ->
+                dialog.cancel()
+            }
+            .setNegativeButton(R.string.cancel_alert_dialog){ dialog, _ ->
+                dialog.cancel()
+            }
+            .create()
+            .show()
     }
 }
